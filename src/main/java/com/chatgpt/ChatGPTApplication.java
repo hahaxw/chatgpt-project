2e6daf654b7101c44c83999140c120210b446289
我de <2270506143@qq.com>
2023-05-04 23:15:33 +0800

    chatGPT

---
+++ src/main/java/com/chatgpt/ChatGPTApplication.java
@@ -0,0 +1,15 @@
+package com.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by vn0790t on 2020/1/9.
+ */
+@SpringBootApplication
+public class ChatGPTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGPTApplication.class, args);
+    }
+}
